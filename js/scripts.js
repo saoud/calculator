feecf26dc631b3f3b31d53fa481645a0057ba3ea
Saoud <saoud@saoud.dev>
2021-01-20 19:11:38 -0800

    updating scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,13 @@
-function add(number1, number2) {
-  return number1 + number2;
-}
+jQuery(document).ready(function() {
+  jQuery("h1").click(function() {
+    alert("This is a header.");
+  });
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
-alert(result);
+  jQuery("p").click(function() {
+    alert("This is a paragraph.");
+  });
 
-jQuery("h1").click(function() {
-  alert("This is a header.");
-});
-
-jQuery("p").click(function() {
-  alert("This is a paragraph.");
-});
-
-jQuery("img").click(function() {
-  alert("This is an image.");
+  jQuery("img").click(function() {
+    alert("This is an image.");
+  });
 });
