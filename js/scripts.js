1129f8461d34965fd89d2e0693c328dda1a333dd
Saoud <saoud@saoud.dev>
2021-01-20 19:09:17 -0800

    adding help-html and changes to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,15 @@ const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 const result = add(number1, number2);
 alert(result);
+
+jQuery("h1").click(function() {
+  alert("This is a header.");
+});
+
+jQuery("p").click(function() {
+  alert("This is a paragraph.");
+});
+
+jQuery("img").click(function() {
+  alert("This is an image.");
+});
